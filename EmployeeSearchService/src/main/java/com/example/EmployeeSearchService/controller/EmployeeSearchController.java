f51896ad6fef07e104b45cf80107e571e75e1c7b
ckabiju <ckabiju@gmail.com>
2021-05-17 17:44:50 -0600

    Added endpoint /text to return server port

--- EmployeeSearchService/src/main/java/com/example/EmployeeSearchService/controller/EmployeeSearchController.java
+++ EmployeeSearchService/src/main/java/com/example/EmployeeSearchService/controller/EmployeeSearchController.java
@@ -3,6 +3,7 @@ package com.example.EmployeeSearchService.controller;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,9 @@ public class EmployeeSearchController {
 
 	@Autowired
 	 EmployeeSearchService employeeSearchService;
+	
+	@Autowired
+	private ServerProperties serverProperties;
 
 	 @RequestMapping("/employee/find/{id}")
 	 public Employee findById(@PathVariable Long id) {
@@ -26,5 +30,10 @@ public class EmployeeSearchController {
 	 @RequestMapping("/employee/findall")
 	 public Collection < Employee > findAll() {
 	  return employeeSearchService.findAll();
+	 }
+	 
+	 @RequestMapping("/employee/text")
+	 public String text() {
+	  return serverProperties.getPort().toString();
 	 }
 }
